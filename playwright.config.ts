43de31c1c59ff9e7254301d4b4b293bff1f82090
THASSO <thassokmorais@gmail.com>
2026-01-17 17:29:43 -0300

    config

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', { open: 'never' }]],
+    reporter: [
+    ['list'],
+    ['html', { open: 'never' }],
+  ],
 
 
   use: {
