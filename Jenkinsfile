b07a446be9ec415e3790bde27149f57fde071ce0
THASSO <thassokmorais@gmail.com>
2026-01-17 15:47:01 -0300

    Pipe teste

---
+++ Jenkinsfile
@@ -0,0 +1,17 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Node.js Deps') {
+            steps {
+                sh 'npm install'
+            }
+        }
+
+        stage('Testes') {
+            steps {
+                sh 'npx playwright tests'
+            }
+        }
+    }
+}
--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-
+  
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -24,18 +24,18 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['list'],
-    ['html'],
-    ['allure-playwright']
+    ['list'],                
+    ['html'],                 
+    ['allure-playwright']     
   ],
-
+  
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-
+  
   },
 
   /* Configure projects for major browsers */
@@ -45,6 +45,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
